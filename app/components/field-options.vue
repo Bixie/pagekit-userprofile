3bf454ce1ecf8d77ff491e9716605e81868588cc
malles <matthijs@bixie.nl>
2015-07-30 04:26:10 +0200

    attachValue

--- app/components/field-options.vue
+++ app/components/field-options.vue
@@ -26,6 +26,7 @@
                 this.field.options.push({
                     value: '',
                     text: '',
+                    attachValue: true,
                     invalid: false
                 });
             },
@@ -72,13 +73,20 @@
 
             selectoption: {
 
-                template: '<li class="uk-nestable-item" data-value="{{ selectoption.value }}">\n    <div class="uk-nestable-panel uk-visible-hover uk-form uk-flex uk-flex-middle">\n        <div class="uk-flex-item-1">\n            <div class="uk-form-row">\n                <small class="uk-form-label uk-text-muted uk-text-truncate" style="text-transform: none" v-class="uk-text-danger: selectoption.invalid">{{ selectoption.value }}</small>\n                <div class="uk-form-controls">\n                    <input type="text" class="uk-form-width-large" v-model="selectoption.text"/></div>\n                <p class="uk-form-help-block uk-text-danger" v-show="selectoption.invalid">{{ selectoption.invalid | trans }}</p>\n\n            </div>\n        </div>\n        <div class="">\n            <ul class="uk-subnav pk-subnav-icon">\n                <li><a class="pk-icon-delete pk-icon-hover uk-invisible" v-on="click: deleteFieldoption($index)"></a></li>\n                <li><a class="pk-icon-move pk-icon-hover uk-invisible uk-nestable-handle"></a></li>\n            </ul>\n        </div>\n    </div>\n</li>   \n',
+                template: '<li class="uk-nestable-item" data-value="{{ selectoption.value }}">\n    <div class="uk-nestable-panel uk-visible-hover uk-form uk-flex uk-flex-middle">\n        <div class="uk-flex-item-1">\n            <div class="uk-form-row">\n                <small class="uk-form-label uk-text-muted uk-text-truncate" style="text-transform: none"\n                       v-show="selectoption.attachValue"\n                       v-class="uk-text-danger: selectoption.invalid">{{ selectoption.value }}</small>\n                <span class="uk-form-label" v-show="!selectoption.attachValue">\n                    <input type="text" class="uk-form-small"\n                           v-on="keyup: safeValue(true)"\n                           v-class="uk-text-danger: selectoption.invalid"\n                           v-model="selectoption.value"/></span>\n                <div class="uk-form-controls">\n                    <input type="text" class="uk-form-width-large" v-model="selectoption.text"/></div>\n                <p class="uk-form-help-block uk-text-danger" v-show="selectoption.invalid">{{ selectoption.invalid | trans }}</p>\n\n            </div>\n        </div>\n        <div class="">\n            <ul class="uk-subnav pk-subnav-icon">\n                <li><a class="uk-icon uk-margin-small-top pk-icon-hover uk-invisible"\n                       data-uk-tooltip="{delay: 500}" title="{{ \'Link/Unlink value from label\' | trans}}"\n                       v-class="uk-icon-link: !selectoption.attachValue, uk-icon-chain-broken: selectoption.attachValue"\n                       v-on="click: selectoption.attachValue = !selectoption.attachValue"></a></li>\n                <li><a class="pk-icon-delete pk-icon-hover uk-invisible" v-on="click: deleteFieldoption($index)"></a></li>\n                <li><a class="pk-icon-move pk-icon-hover uk-invisible uk-nestable-handle"></a></li>\n            </ul>\n        </div>\n    </div>\n</li>   \n',
 
                 inherit: true,
 
+                methods: {
+                    safeValue: function (checkDups) {
+                        this.selectoption.value = _.escape(_.snakeCase(this.selectoption.value));
+                        if (checkDups) this.checkDuplicates();
+                    }
+                },
+
                 watch: {
                     "selectoption.text": function(value) {
-                        this.selectoption.value = _.escape(_.snakeCase(value));
+                        if (this.selectoption.attachValue) this.selectoption.value = _.escape(_.snakeCase(value));
                         this.checkDuplicates();
                     }
 
