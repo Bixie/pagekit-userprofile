7727d8fcdb1ee1c8398777874858e9b4701c1d76
malles <matthijs@bixie.nl>
2015-12-12 21:35:27 +0100

    data in options when inited

--- app/components/appearance.vue
+++ app/components/appearance.vue
@@ -42,7 +42,7 @@
 
         computed: {
             appearanceSettings: function () {
-                return this.field.type ? Profilefields.components[this.field.type].appearance : {};
+                return this.field.type ? Profilefields.components[this.field.type].appearance || Profilefields.components[this.field.type].options.appearance : {};
             }
         }
 
