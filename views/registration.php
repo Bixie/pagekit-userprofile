647a37310a2e9feefe9088b003274338a30224d6
malles <matthijs@bixie.nl>
2015-12-13 13:17:31 +0100

    fix submit

--- views/registration.php
+++ views/registration.php
@@ -1,6 +1,6 @@
 <?php $view->script('userprofile-registration', 'bixie/userprofile:app/bundle/registration.js', ['vue', 'userprofile-profilefields', 'uikit-form-password']) ?>
 
-<form id="userprofile-registration" class="uk-article uk-form uk-form-stacked" name="form" v-validator="form" v-on="submit: save | valid" v-cloak>
+<form id="userprofile-registration" class="uk-article uk-form uk-form-stacked" name="form" v-validator="form" @submit.prevent="save | valid" v-cloak>
 	<div class="uk-grid">
 		<div class="uk-width-medium-3-4">
 			<h1 class="uk-article-title">{{ 'Registration' | trans }}</h1>
